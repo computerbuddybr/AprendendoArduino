42dea99925e7a2b86ff4c2243ed44f107f130e11
Adriana Cerdeira - Computer Buddy BR <adriana.cerdeira@gmail.com>
2022-12-28 18:22:53 -0300

    Update ContadorSimples.ino

--- Parte3-Displays/ContadorSimples/ContadorSimples.ino
+++ Parte3-Displays/ContadorSimples/ContadorSimples.ino
@@ -1,31 +1,50 @@
-//Definindo pinos
-#define SEG_A 2
-#define SEG_B 3
-#define SEG_C 4
-#define SEG_D 5
-#define SEG_E 6
-#define SEG_F 7
-#define SEG_G 8
+//Struct para displays
+typedef struct Display {
+  char name;
+  int pin_number;
+} ;
 
+//Struct para sequência de valores para ligar os leds corretos para cada número
+typedef struct CountNumber {
+  char number[6];
+  int values_sequence[7];
 
-//Definindo listas com os valores para cada número
-int number_values[10][7] = {{HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, LOW}, {LOW, HIGH, HIGH, LOW, LOW, LOW, LOW}, {HIGH, HIGH, LOW, HIGH, HIGH, LOW, HIGH}, {HIGH, HIGH, HIGH, HIGH, LOW, LOW, HIGH}, {LOW, HIGH, HIGH, LOW, LOW, HIGH, HIGH}, {HIGH, LOW, HIGH, HIGH, LOW, HIGH, HIGH}, {HIGH, LOW, HIGH, HIGH, HIGH, HIGH, HIGH}, {HIGH, HIGH, HIGH, LOW, LOW,LOW, LOW}, {HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH},{HIGH, HIGH, HIGH, HIGH, LOW, HIGH, HIGH}};
+};
 
+//Definindo a variável para o display
+struct Display first_display[7] = {
+  {'A', 2},
+  {'B', 3},
+  {'C', 4},
+  {'D', 5},
+  {'E', 6},
+  {'F', 7},
+  {'G', 8}
+
+}; 
+//Definindo lista com os valores para cada número
+struct CountNumber led_values[10] ={
+  {"zero", {HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, LOW} }, 
+  {"one", {LOW, HIGH, HIGH, LOW, LOW, LOW, LOW}}, 
+  {"two", {HIGH, HIGH, LOW, HIGH, HIGH, LOW, HIGH}}, 
+  {"three", {HIGH, HIGH, HIGH, HIGH, LOW, LOW, HIGH}}, 
+  {"four", {LOW, HIGH, HIGH, LOW, LOW, HIGH, HIGH}}, 
+  {"five", {HIGH, LOW, HIGH, HIGH, LOW, HIGH, HIGH}}, 
+  {"six", {HIGH, LOW, HIGH, HIGH, HIGH, HIGH, HIGH}}, 
+  {"seven", {HIGH, HIGH, HIGH, LOW, LOW,LOW, LOW}}, 
+  {"eight", {HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH}},
+  {"nine", {HIGH, HIGH, HIGH, HIGH, LOW, HIGH, HIGH}}
+};
 
 /*
 Função que vai ligar os segmentos apropriados
 */
-void number(int values[]){
-  digitalWrite(SEG_A, values[0]);
-  digitalWrite(SEG_B, values[1]);
-  digitalWrite(SEG_C, values[2]);
-  digitalWrite(SEG_D, values[3]);
-  digitalWrite(SEG_E, values[4]);  
-  digitalWrite(SEG_F, values[5]);  
-  digitalWrite(SEG_G, values[6]); 
-  delay(1000);
-  
+void number(struct CountNumber number_led_values[], struct Display display[], int number){
+ for(int i = 0; i < 7; i++){
+  digitalWrite(display[i].pin_number, number_led_values[number].values_sequence[i]);
+ }
 }
+
 /*
 Função para contar os números
 */
@@ -33,25 +52,31 @@ void counter(){
   Serial.println("Counter");
     for(int i = 0; i < 10; i++){
         Serial.println(i);
-        number(number_values[i]);        
+        number(led_values, first_display, i);
+        delay(1000);        
     }
 }
 
+/*
+Função para o setup do display
+*/
+void display_setup(struct Display display[]){
+  for(int i = 0; i < 7; i++){
+    pinMode(display[i].pin_number, OUTPUT);
+    Serial.println(display[i].pin_number);
+  }
+
+}
 
 
 /*
 Funções de funcionamento da placa
 */
 void setup(){
-    pinMode(SEG_A, OUTPUT);
-    pinMode(SEG_B, OUTPUT);
-    pinMode(SEG_C, OUTPUT);
-    pinMode(SEG_D, OUTPUT);
-    pinMode(SEG_E, OUTPUT);
-    pinMode(SEG_F, OUTPUT);
-    pinMode(SEG_G, OUTPUT);
     Serial.begin(9600);
     Serial.println("Setup");
+    display_setup(first_display);
+    
 
 }
 void loop(){   
